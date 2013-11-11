105fffae02afb083586cd1e2a5fd9a633046c415
André Gaul <gaul@web-yard.de>
2013-11-11 01:19:03 +0100

    add filter

---
+++ ddoc/filters/by_id_or_bbox.js
@@ -0,0 +1,8 @@
+function(doc, req){
+  var common = require('views/lib/couchmap-common');
+
+  if (doc.type!="router") {
+    return common.filter_id_or_bbox(doc, req);
+  }
+  return false;
+}
--- src/js/couchmap.js
+++ src/js/couchmap.js
@@ -1,4 +1,6 @@
 var $ = require('jquery');
+var Backbone =  require('backbone');
+Backbone.$ = $;
 $(document).ready(function() {
   var L = require('leaflet');
   // create map and add OpenStreetMap tile layer
@@ -10,13 +12,15 @@ $(document).ready(function() {
   var CouchMapModel = require('couchmap-backbone/models/couchMap');
   var couchmap = new CouchMapModel({
     coarse_url: 'http://couch.libremap.net/couchmap-dev/_design/couchmap-api/_view/coarse',
-    fine_url: 'http://couch.libremap.net/couchmap-dev/_design/couchmap-api/_spatial/by_location'
+    fine_url: 'http://couch.libremap.net/couchmap-dev/_design/couchmap-api/_spatial/by_location',
+    changes_url: 'http://couch.libremap.net/couchmap-dev/_changes',
+    changes_filter: 'couchmap-api/by_id_or_bbox'
   });
 
+  // TODO: move into view
   couchmap.on('busy', function() { console.log('busy');});
   couchmap.on('idle', function() { console.log('idle');});
 
-  // TODO: move
   function bounds2bbox(bounds) {
     var sw = bounds.getSouthWest();
     var ne = bounds.getNorthEast();
