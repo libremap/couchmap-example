156c77cc5f8e6f7a2633143106a00ecb209a6da5
André Gaul <gaul@web-yard.de>
2013-11-12 20:36:01 +0100

    add random data

--- src/js/couchmap.js
+++ src/js/couchmap.js
@@ -17,4 +17,29 @@ $(document).ready(function() {
 
   var CouchMapView = require('./views/couchMap');
   var couchmap_view = new CouchMapView({el: $('div.map'), model: couchmap_model});
+
+  // insert random models into DB
+  $('#random_go').on('click', function() {
+    var count = parseInt($('#random_count').val(), 10);
+    console.log(count);
+    var db = 'http://couch.libremap.net/couchmap-dev/';
+    var bounds = couchmap_view.map.getBounds(),
+        ne = bounds.getNorthEast(),
+        sw = bounds.getSouthWest(),
+        docs = [];
+    for (var i=0; i<count; i++) {
+      docs.push({
+        lat: sw.lat + Math.random()*(ne.lat-sw.lat),
+        lon: sw.lng + Math.random()*(ne.lng-sw.lng)
+      });
+    }
+    $.ajax({
+      type: 'POST',
+      dataType: 'json',
+      contentType: 'application/json',
+      data: JSON.stringify({'docs': docs}),
+      url: db+'/_bulk_docs',
+      success: function () { console.log('pushed '+count+' documents'); }
+    });
+  });
 });
