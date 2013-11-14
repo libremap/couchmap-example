a6ca3d07e36de3574c54aefed1637846a97bbf7b
André Gaul <gaul@web-yard.de>
2013-11-14 22:05:54 +0100

    include bbox in coarse map-reduce

--- ddoc/views/coarse/map.js
+++ ddoc/views/coarse/map.js
@@ -3,7 +3,15 @@ function(doc) {
   if (doc.lat && doc.lon) {
     var keys = common.coarse_map_keys(doc.lat, doc.lon);
     common._.each(keys, function(k) {
-      emit(k, {count: 1, lat: doc.lat, lon: doc.lon});
+      emit(k, {
+        count: 1,
+        lat: doc.lat,
+        lon: doc.lon,
+        bbox_west: doc.lon,
+        bbox_east: doc.lon,
+        bbox_south: doc.lat,
+        bbox_north: doc.lat
+      });
     });
   }
 }
